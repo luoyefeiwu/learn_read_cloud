415f1a3c9347ac4b4913403147917c7ad98f6d5e
HCW <568384493@qq.com>
2020-11-03 23:56:01 +0800

    首页

--- book-cloud-homepage/src/main/java/com/jerry/bookcloud/homepage/controller/IndexPageConfigController.java
+++ book-cloud-homepage/src/main/java/com/jerry/bookcloud/homepage/controller/IndexPageConfigController.java
@@ -1,14 +1,31 @@
 package com.jerry.bookcloud.homepage.controller;
 
-import io.swagger.annotations.Api;
+import com.jerry.bookcloud.common.result.Result;
+import com.jerry.bookcloud.homepage.service.IndexPageConfigService;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Api(description = "精品接口")
 @RestController
 public class IndexPageConfigController {
+    @Autowired
+    private IndexPageConfigService indexPageConfigService;
 
+    @ApiOperation(value = "获取精品页接口", httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "type", value = "类型：1.主页，2.男生，3.女生", required = true, dataType = "Integer"),
+            @ApiImplicitParam(paramType = "query", name = "page", value = "页数", required = true, dataType = "Integer"),
+            @ApiImplicitParam(paramType = "query", name = "limit", value = "每页数量", required = true, dataType = "Integer")
+    })
+    @ApiResponses({@ApiResponse(code = 200, message = "", response = String.class)})
+    @GetMapping("index")
+    public Result getIndexPageByType(Integer type, Integer page, Integer limit) {
+        return this.indexPageConfigService.getIndexPageByType(type, page, limit);
+    }
 
 }
